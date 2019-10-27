05efc97472289c1d7fe07f5b64f74deb71f3403e
Endrju <s12771@pjwstk.edu.pl>
2019-10-27 19:02:29 +0100

    bit smoother

--- src/main/scala/Response.scala
+++ src/main/scala/Response.scala
@@ -1,8 +1,7 @@
 import spray.json.DefaultJsonProtocol
 
-
 case class Response(kind: String, message: Option[String], index: Option[Int], radicand: Option[Int])
 
 trait MyJsonProtocol extends DefaultJsonProtocol {
-  implicit val mThreeFormat = jsonFormat4(Response)
+  implicit val responseFormat = jsonFormat4(Response)
 }
